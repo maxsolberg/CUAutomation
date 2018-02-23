f8e04f94c64dcb13d8eeeb30773acc9a23a24634
Jacob D. Johansen <jacobd.johansen@dhcp-clara-7039.eduroam.cornell.edu>
2018-02-23 00:11:42 -0500

    trapezoidarea.py created

---
+++ PythonFiles/trapezoidarea.py
@@ -0,0 +1,8 @@
+# Area of a Trapezoid
+# By Jacob Johansen
+
+A= float(input("base 1 of trapezoid: "))
+B= float(input("base 2 of trapezoid: "))
+C= float (input("height of trapezoid: "))
+D= (A+B)*0.5*C
+print("area of trapezoid is: " + str (D))
