288a2442f1615be51670007faf209b99db0aa33f
Max Solberg <mrs372@cornell.edu>
2018-02-22 21:33:56 -0500

    wrote the code and it works

--- PythonFiles/Quadratic_Formula.py
+++ PythonFiles/Quadratic_Formula.py
@@ -1,1 +1,12 @@
 # Quadratic Formula
+# Created by Max Solberg
+
+import math
+
+print("Input A, B, and C for Ax^2+Bx+C=0")
+A= int(input("A: "))
+B= int(input("B: "))
+C= int(input("C: "))
+D= (-B+math.sqrt(B**2-4*A*C))/(2*A)
+E= (-B-math.sqrt(B**2-4*A*C))/(2*A)
+print("The solutions to the equation " + str(A) + "x^2+" + str(B) + "x+" + str(C) + " are: " + str(D) + " and " + str(E))
