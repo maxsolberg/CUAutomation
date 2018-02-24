0d0a7fcbefea69246ecd585659127b997244bbda
zmr5 <36309454+zmr5@users.noreply.github.com>
2018-02-23 20:16:20 -0500

    Add files via upload
    Vector class

---
+++ PythonFiles/Zaeem Drop Here/Vector.py
@@ -0,0 +1,69 @@
+class Vector(object):
+    
+    """
+    Creates the a new Vector with x,y,z
+    
+    x,y,z are defaulted to 0
+    
+    Parameter x: x for x postion
+    Precondition: x is a any number
+    
+    Parameter y: y for z postion
+    Precondition: y is a any number
+    
+    Parameter z: z for z postion
+    Precondition: z is a any number
+    """
+    def __init__(self, x=0, y= 0, z=0):
+        self._x = x
+        self._y = y
+        self._z = z
+    
+    """
+    Just Getters for the components
+    """
+    def getX():
+        return self._x
+    def getY():
+        return self._y
+    def getZ():
+        return self._z
+    
+    """
+    Just setters for the components.
+    Precondition: x,y,z are numbers
+    """
+    def setX(self, x);
+        self._x = x
+    def setY(self, y);
+        self._y = y
+    def setZ(self, z);
+        self._z = z
+    
+    """
+    Return the magnitude of the vector
+    """
+    def magnitude():
+        return (x**2 + y**2 + z**2)**0.5
+    
+    """
+    Return the dot Product of this Vector and Vector v
+    
+    Parameter: v is a Vector to conduct a dot product with
+    Parameter: v is a Vector class
+    """
+    def dotProduct(self, v):
+        return self.getX() * v.getX() + self.getY() * v.getY() + self.getZ() * v.getZ()
+    
+    """
+    Return the cross Product of this Vector and Vector v
+    
+    Parameter: v is a Vector to conduct a cross product with
+    Parameter: v is a Vector class
+    """
+    def crossProduct(self, v):
+        newX = self.getY() * v.getZ() - self.getZ() * v.getY()
+        newY = self.getX() * v.getZ() - self.getZ() * v.getX()
+        newZ = self.getX() * v.getY() - self.getY() * v.getX()
+        
+        return Vector(newX, newY, newZ)
