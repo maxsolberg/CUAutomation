2fc9734caaa1f42a02e66a7e3a6749d02b830303
Max Solberg <mrs372@cornell.edu>
2018-05-04 23:46:58 -0400

    edge and node classes perfected :D

--- PythonFiles/OR Graph/edge.py
+++ PythonFiles/OR Graph/edge.py
@@ -9,18 +9,24 @@ class Edge():
         weight (float): numerical value associated the edge. If True, the value
             is equal to the euclidian distance between the two nodes. Otherwise
             it is equal to the given value.
-        directedness: bool, true if the edge is part of an undirected graph and
-            therefore has no specified direction, false if the edge is part of
+        directed: bool, false if the edge is part of an undirected graph and
+            therefore has no specified direction, true if the edge is part of
             a directed graph which constitutes that the edge starts at the start
             node and ends at the end node."""
 
-    def __init__(self, start, end, weight):
+    def __init__(self, start, end, weight=None, directed=False):
         self.start_node = start
         self.end_node = end
-        self.weight = self.start_node.distanceto(self.end_node) if weight == True else weight
-        # self.directedness = direct
+        self.weight = self.start_node.distanceto(self.end_node) if weight == None else weight
+        self.directed = directed
+        self.start_node.addneighbor(self.end_node, self)
+        if directed == False:
+            self.end_node.addneighbor(self.start_node, self)
 
     def __str__(self):
         n1 = self.start_node
         n2 = self.end_node
-        return "edge from " + str(n1) + " to " + str(n2) + " of weight " + str(self.weight)
+        if self.directed == True:
+            return "edge of weight " + str(self.weight) + " connecting " + str(n1) + " to " + str(n2)
+        else:
+            return "edge of weight " + str(self.weight) + " connecting " + str(n1) + " and " + str(n2)
--- PythonFiles/OR Graph/node.py
+++ PythonFiles/OR Graph/node.py
@@ -7,12 +7,26 @@ class Node():
         x: the x-coordinate of the Node
         y: the y-coordinate of the Node"""
 
-    def __init__(self, x, y):
+    def __init__(self, x=0, y=0, value=1, name=None):
         self.x = x
         self.y = y
+        self.value = value
+        self.name = name if name != None else "Node at (" + str(self.x) + ", " + str(self.y) + ") of value " + str(self.value)
+        self.neighbors = []
+        self.edges = []
 
     def __str__(self):
-        return "Node at (" + str(self.x) + ", " + str(self.y) + ")"
+        if self.name == "Node at (" + str(self.x) + ", " + str(self.y) + ") of value " + str(self.value):
+            return self.name
+        else:
+            return str(self.name) + ": Node at (" + str(self.x) + ", " + str(self.y) + ") of value " + str(self.value)
 
     def distanceto(self, other):
         return math.sqrt((self.x-other.x)**2 + (self.y-other.y)**2)
+
+    def addneighbor(self, other, edge):
+        self.neighbors.append(other)
+        self.edges.append(edge)
+
+    def coordinates(self):
+        return "(" + str(self.x) + ", " + str(self.y) + ")"
